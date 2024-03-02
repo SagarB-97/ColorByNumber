3e8f8f6d9a49519ae6b099ff3d056a83e196c5b9
Sagar Bharadwaj <skalasib@andrew.cmu.edu>
2024-03-02 18:37:20 -0500

    Updated Utils to accepts cmap

--- colorbynumber/utils.py
+++ colorbynumber/utils.py
@@ -1,6 +1,10 @@
 from matplotlib import pyplot as plt
 
-def show_image(image):
-    plt.imshow(image)
+def show_image(image, cmap = None):
+    if cmap:
+        plt.imshow(image, cmap = cmap)
+    else:
+        plt.imshow(image)
     plt.axis('off')
     plt.show()
+
